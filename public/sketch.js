3b63ee8842392ece5541ba86c2d0cab1333cf5e1
Soumya Mondal <iamsammyyo@gmail.com>
2021-05-29 12:12:20 +0530

    Update sketch.js

--- public/sketch.js
+++ public/sketch.js
@@ -1,68 +1,31 @@
+// Flocking
 // Daniel Shiffman
-// http://codingtra.in
-// http://patreon.com/codingtrain
-// Code for: https://youtu.be/BjoM9oKOAKY
+// https://thecodingtrain.com
 
-var inc = 0.1;
-var scl = 10;
-var cols, rows;
-var canvas;
+// https://thecodingtrain.com/CodingChallenges/124-flocking-boids.html
+// https://youtu.be/mhjuuHl6qHM
+// https://editor.p5js.org/codingtrain/sketches/ry4XZ8OkN
 
-var zoff = 0;
+const flock = [];
 
-var fr;
-
-var particles = [];
-
-var flowfield;
+let alignSlider, cohesionSlider, separationSlider;
 
 function setup() {
-  canvas = createCanvas(windowWidth, windowHeight);
-  canvas.position(0,0);
-  canvas.style('z-index','-1');
-  colorMode(HSB, 255);
-  cols = floor(width / scl);
-  rows = floor(height / scl);
-  fr = createP('');
-
-  flowfield = new Array(cols * rows);
-
-  for (var i = 0; i < 300; i++) {
-    particles[i] = new Particle();
+  createCanvas(640, 360);
+  alignSlider = createSlider(0, 2, 1, 0.1);
+  cohesionSlider = createSlider(0, 2, 1, 0.1);
+  separationSlider = createSlider(0, 2, 1, 0.1);
+  for (let i = 0; i < 200; i++) {
+    flock.push(new Boid());
   }
-  background(255);
 }
 
 function draw() {
-  var yoff = 0;
-  for (var y = 0; y < rows; y++) {
-    var xoff = 0;
-    for (var x = 0; x < cols; x++) {
-      var index = x + y * cols;
-      var angle = noise(xoff, yoff, zoff) * TWO_PI * 4;
-      var v = p5.Vector.fromAngle(angle);
-      v.setMag(1);
-      flowfield[index] = v;
-      xoff += inc;
-      stroke(0, 50);
-      // push();
-      // translate(x * scl, y * scl);
-      // rotate(v.heading());
-      // strokeWeight(1);
-      // line(0, 0, scl, 0);
-      // pop();
-    }
-    yoff += inc;
-
-    zoff += 0.0003;
+  background(51);
+  for (let boid of flock) {
+    boid.edges();
+    boid.flock(flock);
+    boid.update();
+    boid.show();
   }
-
-  for (var i = 0; i < particles.length; i++) {
-    particles[i].follow(flowfield);
-    particles[i].update();
-    particles[i].edges();
-    particles[i].show();
-  }
-
-  fr.html(floor(frameRate()));
 }
